ccfb9949f5ef0b59053462706b26c428ec2f769b
Anabizo <aninhadealmeida0502@gmail.com>
2024-06-03 11:06:22 -0300

    modifying texts and images

--- source/pages/Home/problems-faced-by-beginners.tsx
+++ source/pages/Home/problems-faced-by-beginners.tsx
@@ -3,23 +3,19 @@ import ListingFactory from "~/Components/molecules/listing-factory";
 export const ProblemsFacedByBeginners = () => {
     const problems: Array<string> = [
         `Estabelecimento de Clientela: Marketing e networking são 
-        essenciais para atrair os primeiros clientes.`,
+        essenciais para construir reputação e atrair os primeiros clientes, 
+        competindo com clínicas estabelecidas.`,
 
         `Falta de Experiência Prática:  Ganhar confiança após a 
         formação é fundamental para lidar com casos diversos.
     `,
-        `Administração do Negócio: Gestão financeira, contabilidade 
-        e gerenciamento de funcionários são desafios comuns enfrentados`,
+        `Administração do Negócio: Gestão financeira, contabilidade, 
+        gerenciamento de funcionários e equilíbrio entre  trabalho e 
+        vida pessoal são desafios comuns enfrentados, principalmente 
+        por iniciantes`,
 
-        `Equilíbrio Trabalho-Vida: O campo veterinário exige longas
-horas de trabalho, afetando o equilíbrio pessoal,
-especialmente para iniciantes.
-`,
         `Investimentos em Equipamentos: Equipamentos veterinários são 
         caros e representam um contratempo financeiro para esses profissionais.
-`,
-        `Concorrência com Clínicas: Competir com clínicas estabelecidas 
-        requer esforços adicionais para construir reputação.
 `,
     ];
 
