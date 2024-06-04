09412f3cbc316c708ebae04e309627bc0c652f00
Murilo Montino <51464383+murilomontino@users.noreply.github.com>
2024-06-04 20:37:00 -0300

    fix: correções nas chamadas de links e efeito em botões. (#23)
    * refactor: remove componente Header não utilizado.
    
    * refactor: corrido path das landing-pages auxiliares.
    
    * fix: update links em nav-bar.

--- source/pages/Home/HomePage.tsx
+++ source/pages/Home/HomePage.tsx
@@ -1,42 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
+'use client'
 
-import { BtnLink } from "~/Components/atoms/btn";
+import { BtnLink } from '~/Components/atoms/btn'
 
-import { FaStethoscope } from "react-icons/fa";
-import { MdPerson } from "react-icons/md";
-import Slide from "react-reveal/Slide";
-import Zoom from "react-reveal/Zoom";
-import HeaderTitle from "~/Components/atoms/header-title";
-import NavbarHome from "~/Components/molecules/nav-bar-home";
-import Footer from "~/Layouts/Footer";
-import ListingFactory from "~/Components/molecules/listing-factory/listing-factory";
-import { Solutions } from "./solutions";
-import { BusinessDifferentials } from "./business-differentials";
-import { ProblemsFacedByBeginners } from "./problems-faced-by-beginners";
-import { ProblemsFacedByVeterinarians } from "./problems-faced-by-veterinarians";
+import { FaStethoscope } from 'react-icons/fa'
+import { MdPerson } from 'react-icons/md'
+import Slide from 'react-reveal/Slide'
+import Zoom from 'react-reveal/Zoom'
+import HeaderTitle from '~/Components/atoms/header-title'
+import NavbarHome from '~/Components/molecules/nav-bar-home'
+import Footer from '~/Layouts/Footer'
+import { ProblemsFacedByBeginners } from './problems-faced-by-beginners'
+import { ProblemsFacedByVeterinarians } from './problems-faced-by-veterinarians'
+import { Solutions } from './solutions'
 
 export const ButtonsNavBar = () => {
     return (
         <div className="flex items-center justify-center w-full gap-2">
             <BtnLink
                 message="Saiba + Tutor"
-                className="text-gray-500 hover:!bg-secondary-500 !border-secondary-500 border-0 w-32 mobile:border-0"
-                href="/tutor/landing-page"
+                className="text-gray-500  !border-secondary-500 border-0 w-32 mobile:border-0"
+                href="/tutor"
             >
                 <MdPerson className="w-6 h-6" />
             </BtnLink>
             <BtnLink
                 message="Saiba + Vet"
-                className="border-primary-600 hover:!bg-secondary-500 border-0 w-32 mobile:border-0"
-                href="/veterinary/landing-page"
+                className="w-32 border-0 border-primary-600 mobile:border-0"
+                href="/veterinary"
             >
                 {/* icon de cadastro */}
                 <FaStethoscope className="w-6 h-4" />
             </BtnLink>
         </div>
-    );
-};
+    )
+}
 
 const HomePage = () => {
     return (
@@ -75,12 +73,12 @@ const HomePage = () => {
                 </div>
             </section>
 
-            <section className="section pb-0 hero-section bg-white">
+            <section className="pb-0 bg-white section hero-section">
                 <div className="bg-overlay bg-overlay-pattern" />
-                <div className="grid grid-cols-1 place-items-center md:mt-0 md:grid-cols-2 p-4">
+                <div className="grid grid-cols-1 p-4 place-items-center md:mt-0 md:grid-cols-2">
                     <Zoom>
-                        <div className="flex items-center text-center col-span-1">
-                            <p className="text-lg lg:text-2xl font-semibold font-sans p-4">
+                        <div className="flex items-center col-span-1 text-center">
+                            <p className="p-4 font-sans text-lg font-semibold lg:text-2xl">
                                 {`
                                   Nossa startup revoluciona o mercado, oferecendo uma plataforma 
                                   exclusiva para médicos veterinários domiciliares e iniciantes. 
@@ -119,10 +117,10 @@ const HomePage = () => {
                 </div>
             </section>
 
-            <section className="section pb-0 hero-section bg-white">
+            <section className="pb-0 bg-white section hero-section">
                 <div className="bg-overlay bg-overlay-pattern" />
-                <div className="grid grid-cols-1 place-items-center md:mt-0 md:grid-cols-2 p-4">
-                    <div className="flex items-center text-center col-span-1">
+                <div className="grid grid-cols-1 p-4 place-items-center md:mt-0 md:grid-cols-2">
+                    <div className="flex items-center col-span-1 text-center">
                         <ProblemsFacedByBeginners />
                     </div>
                     <Slide left>
@@ -175,13 +173,13 @@ const HomePage = () => {
                 </div>
             </section>
 
-            <section className="section pb-0 hero-section bg-white">
+            <section className="pb-0 bg-white section hero-section">
                 <div className="bg-overlay bg-overlay-pattern" />
-                <div className="grid grid-cols-1 place-items-center md:mt-0 md:grid-cols-2 p-4">
+                <div className="grid grid-cols-1 p-4 place-items-center md:mt-0 md:grid-cols-2">
                     <div className="col-span-1 pt-5 text-center mt-lg-5">
                         <Solutions />
                         <Zoom>
-                            <p className="text-lg lg:text-2xl font-semibold font-sans p-4">
+                            <p className="p-4 font-sans text-lg font-semibold lg:text-2xl">
                                 {`
                                 Em resumo, a PawKeepr promove cuidados de qualidade e acessíveis 
                                 por meio de uma abordagem centrada no cliente para o compartilhamento 
@@ -203,14 +201,14 @@ const HomePage = () => {
             </section>
 
             <section className="section bg-primary-500">
-                <h2 className="text-center p-1 font-sans text-lg font-semibold text-white lg:text-2xl">
+                <h2 className="p-1 font-sans text-lg font-semibold text-center text-white lg:text-2xl">
                     Por que usar?
                 </h2>
                 <div className="bg-overlay bg-overlay-pattern" />
                 <div className="grid grid-cols-1 web:grid-cols-2">
                     <Zoom right>
                         <div className="col-span-1 pt-5 mt-5 text-center mt-lg-5">
-                            <div className="bg-secondary rounded-xl w-3/4 shadow-md m-5">
+                            <div className="w-3/4 m-5 shadow-md bg-secondary rounded-xl">
                                 <p className="p-4 font-sans text-lg font-semibold text-white lg:text-2xl">
                                     {`
                                  1. Com nosso app, você terá acesso
@@ -227,7 +225,7 @@ const HomePage = () => {
                         </div>
                     </Zoom>
                     <Slide left>
-                        <div className="col-span-1 hidden lg:block">
+                        <div className="hidden col-span-1 lg:block">
                             <img
                                 src="/bg-three.jpg"
                                 className="w-[75%]"
@@ -248,7 +246,7 @@ const HomePage = () => {
                     </Slide>
                     <Zoom left>
                         <div className="col-span-1 pt-5 text-center mt-lg-5">
-                            <div className="bg-secondary rounded-xl w-3/4 shadow-md m-2">
+                            <div className="w-3/4 m-2 shadow-md bg-secondary rounded-xl">
                                 <p className="p-4 font-sans text-lg font-semibold text-white lg:text-2xl">
                                     {`
                                  2. Nossa plataforma sincroniza 
@@ -266,8 +264,8 @@ const HomePage = () => {
                 <div className="grid grid-cols-1 p-4 web:grid-cols-2">
                     <Zoom right>
                         <div className="col-span-1 pt-5 text-center mt-lg-5">
-                            <div className="bg-secondary rounded-xl w-3/4 shadow-md m-2">
-                                <p className=" p-4 font-sans text-lg font-semibold text-white lg:text-2xl">
+                            <div className="w-3/4 m-2 shadow-md bg-secondary rounded-xl">
+                                <p className="p-4 font-sans text-lg font-semibold text-white lg:text-2xl">
                                     {`
                                  3. Facilitamos a colaboração entre veterinário e
                                  clientes, permitindo que ambos
@@ -301,7 +299,7 @@ const HomePage = () => {
                     </Slide>
                     <Zoom left>
                         <div className="col-span-1 pt-5 text-center mt-lg-5">
-                            <div className="bg-secondary rounded-xl w-3/4 shadow-md m-2">
+                            <div className="w-3/4 m-2 shadow-md bg-secondary rounded-xl">
                                 <p className="p-4 font-sans text-lg font-semibold text-white lg:text-2xl">
                                     {`
                                  4. Investimos significativamente 
@@ -320,8 +318,8 @@ const HomePage = () => {
                 <div className="grid grid-cols-1 p-4 web:grid-cols-2">
                     <Zoom right>
                         <div className="col-span-1 pt-5 text-center mt-lg-5">
-                            <div className="bg-secondary rounded-xl w-3/4 shadow-md m-2">
-                                <p className=" p-4 font-sans text-lg font-semibold text-white lg:text-2xl">
+                            <div className="w-3/4 m-2 shadow-md bg-secondary rounded-xl">
+                                <p className="p-4 font-sans text-lg font-semibold text-white lg:text-2xl">
                                     {`
                                  5 .Com uma interface intuitiva e 
                                 amigável, o PawKeepr oferece uma 
@@ -347,37 +345,37 @@ const HomePage = () => {
                 </div>
             </section>
 
-            <section className="section pb-0 hero-section bg-white">
+            <section className="pb-0 bg-white section hero-section">
                 <div className="grid grid-cols-1 p-4 web:grid-cols-2">
                     <Zoom>
                         <div className="col-span-1 pt-5 text-center mt-lg-5">
                             <p className="p-2 font-sans text-lg font-semibold lg:text-2xl">
                                 {`Roadmap`}
                             </p>
-                            <div className="w-3/6 flex justify-start">
-                                <div className="bg-secondary rounded-xl shadow-md m-2">
+                            <div className="flex justify-start w-3/6">
+                                <div className="m-2 shadow-md bg-secondary rounded-xl">
                                     <p className="p-3 font-sans text-lg font-semibold text-white lg:text-xl">
                                         {`Inteligência Artificial Abril de 2025`}
                                     </p>
                                 </div>
                             </div>
-                            <div className="w-4/6 flex justify-end">
-                                <div className="bg-secondary rounded-xl shadow-md m-2">
-                                    <p className=" p-3 font-sans text-lg font-semibold text-white lg:text-xl">
+                            <div className="flex justify-end w-4/6">
+                                <div className="m-2 shadow-md bg-secondary rounded-xl">
+                                    <p className="p-3 font-sans text-lg font-semibold text-white lg:text-xl">
                                         {`VetMovel e Telemedicina Dezembro de 2024`}
                                     </p>
                                 </div>
                             </div>
-                            <div className="w-5/6 flex justify-end">
-                                <div className="bg-secondary rounded-xl shadow-md m-2">
-                                    <p className=" p-3 font-sans text-lg font-semibold text-white lg:text-xl">
+                            <div className="flex justify-end w-5/6">
+                                <div className="m-2 shadow-md bg-secondary rounded-xl">
+                                    <p className="p-3 font-sans text-lg font-semibold text-white lg:text-xl">
                                         {`Secretaria Inteligente Agosto de 2024`}
                                     </p>
                                 </div>
                             </div>
-                            <div className="w-6/6 flex justify-end">
-                                <div className="bg-secondary rounded-xl shadow-md m-2">
-                                    <p className=" p-3 font-sans text-lg font-semibold text-white lg:text-xl">
+                            <div className="flex justify-end w-6/6">
+                                <div className="m-2 shadow-md bg-secondary rounded-xl">
+                                    <p className="p-3 font-sans text-lg font-semibold text-white lg:text-xl">
                                         {`Consultas veterinárias Abril de 2024`}
                                     </p>
                                 </div>
@@ -403,7 +401,7 @@ const HomePage = () => {
                         <div className="col-span-1 rounded-md">
                             <div className="flex justify-center">
                                 <div className="bg-white flex flex-col gap-4 items-center justify-center rounded-lg p-4 w-1/2 h-[400px]">
-                                    <h3 className="lg:text-5xl text-gray-500">
+                                    <h3 className="text-gray-500 lg:text-5xl">
                                         Grátis
                                     </h3>
                                     <p className="lg:text-4xl text-primary">
@@ -422,17 +420,17 @@ const HomePage = () => {
                         <div className="col-span-1">
                             <div className="flex justify-center">
                                 <div className="bg-white flex flex-col gap-4 items-center justify-center rounded-lg p-4 w-1/2 h-[400px]">
-                                    <h3 className="lg:text-5xl text-gray-500">
+                                    <h3 className="text-gray-500 lg:text-5xl">
                                         Premium
                                     </h3>
                                     <p className="lg:text-4xl text-primary">
                                         $ 0,99
                                     </p>
-                                    <p className="lg:text-xl text-gray-700">
+                                    <p className="text-gray-700 lg:text-xl">
                                         Pague menos de 1 dólar por consulta
                                         realizada.
                                     </p>
-                                    <p className="lg:text-md text-gray-700">
+                                    <p className="text-gray-700 lg:text-md">
                                         Tenha acesso a todos os recursos sem
                                         limitação
                                     </p>
@@ -443,7 +441,7 @@ const HomePage = () => {
                 </div>
             </section>
         </>
-    );
-};
+    )
+}
 
-export default HomePage;
+export default HomePage
